82c16009106dfc5183617db770e524ce2d52a98b
Prateek Kher <prateek.kher@gmail.com>
2018-04-03 08:53:42 +0530

    Added Frameworks

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
-const app = angular.module('SimpleBillingApp', []);
+const app = angular.module('simpleBillingApp', ['ngRoute']);
+
+app.directive('appRoot', function(){
+    return {
+        'restrict' : 'E',
+        'template' : '{{title}}'
+    }
+})
+.controller('MainController', function($scope){
+    $scope.title = "Simple Billing App"
+})
